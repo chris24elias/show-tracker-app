e8682c445232c52bd5ef55a5e7844aa9d173ebdc
Chris <chris24elias@gmail.com>
2022-09-16 13:49:17 -0400

    fix async storage error

--- src/api/initFirebase.ts
+++ src/api/initFirebase.ts
@@ -1,9 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { FIREBASE_CONFIG } from "../../env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence } from "firebase/auth/react-native";
 
 let myApp = initializeApp(FIREBASE_CONFIG);
+initializeAuth(myApp, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 
 const auth = getAuth(myApp);
 const firestore = getFirestore(myApp);
